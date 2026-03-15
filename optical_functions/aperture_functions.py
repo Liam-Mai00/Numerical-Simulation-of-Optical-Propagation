0e623cb2e5553f8c5ceeedd03cf9e2958aea3dc5
Liam Mai <p.y.mai00@outlook.com>
2026-03-15 20:52:07 +0000

    Initial Commit

---
+++ optical_functions/aperture_functions.py
@@ -0,0 +1,32 @@
+"""
+aperture_functions.py is a module containing common (aperture) functions found in the appendix of J. Schmidt's book.
+Reference:
+ - Numerical Simulation of Optical Propagation - J. Schmidt (Appendix B MATLAB Code Listings)
+"""
+import numpy as np
+
+def rect(x,D=1):
+    x = np.abs(x)
+    y = (x < D/2).astype(float)
+    y[x == D/2] = 0.5
+    return y
+
+def tri(t):
+    t = np.abs(t)
+    y = np.zeros_like(t)
+    y[t < 1.0] = 1.0 - t[t < 1.0]
+    return y
+
+def circ(x,y,D):
+    """
+    Args:
+        x: Meshgrid of x values
+        y: Meshgrid of y values
+        D: Circle diameter
+    Returns:
+        z: Circular aperture of diameter D
+    """
+    r = np.sqrt(x**2 + y**2)
+    z = (r < D/2).astype(float)
+    z[r == D/2] = 0.5
+    return z
