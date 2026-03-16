186d95980f98c0bc9272480a96a184be25b3d7a9
Liam Mai <p.y.mai00@outlook.com>
2026-03-16 16:29:01 +0000

    Added extra comments and introduced bessel function demo (first kind)

--- optical_functions/aperture_functions.py
+++ optical_functions/aperture_functions.py
@@ -6,12 +6,28 @@ Reference:
 import numpy as np
 
 def rect(x,D=1):
+    """
+    Creates a rectangular function
+    
+    Args:
+        x: Input array
+    Returns:
+        y: Rectangular function output array
+    """
     x = np.abs(x)
     y = (x < D/2).astype(float)
     y[x == D/2] = 0.5
     return y
 
 def tri(t):
+    """
+    Creates a triangular function
+
+    Args:
+        t: Input array
+    Returns:
+        y: Triangular function output array
+    """
     t = np.abs(t)
     y = np.zeros_like(t)
     y[t < 1.0] = 1.0 - t[t < 1.0]
@@ -19,12 +35,14 @@ def tri(t):
 
 def circ(x,y,D):
     """
+    Creates a circular aperture.
+
     Args:
         x: Meshgrid of x values
         y: Meshgrid of y values
         D: Circle diameter
     Returns:
-        z: Circular aperture of diameter D
+        z: Array of aperture with diameter D
     """
     r = np.sqrt(x**2 + y**2)
     z = (r < D/2).astype(float)
